49765128e623cd798110789866f3afd28f0b4a27
Ivan <rightrat42@gmail.com>
2023-08-10 16:36:44 +0300

    deploy4?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+</script>
+
 <h1>Hi!</h1>
-<p>Visit <a href="/Storage">Storage</a></p>
-<p>Visit <a href="/">Home</a></p>
+<p>Visit <a href="{ base }/Storage">Storage</a></p>
+<p>Visit <a href="{ base }/">Home</a></p>
 
 <style>
     :global(body) {
