d0179a28d056e95b55cbf9c53471beea20b83af0
Ivan <rightrat42@gmail.com>
2023-08-11 13:58:38 +0300

    misc

--- gh-pages.js
+++ gh-pages.js
@@ -3,8 +3,8 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
         repo: 'https://github.com/RightRat42/rightrat-stash.git', // Update to point to your repository  
+        branch: 'gh-pages',
         user: {
             name: 'Ivan Smirnov', // update to use your name
             email: 'rightrat42@gmail.com' // Update to use your email
